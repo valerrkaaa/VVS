c4bd46c25061d2d78925e0b8f5fc3b0ea2400bf0
valerrkaaa <val_g_2001@bk.ru>
2021-10-27 09:50:30 +0500

    add fill_array

--- lab1_karimov/lab1_Karimov.py
+++ lab1_karimov/lab1_Karimov.py
@@ -1,15 +1,24 @@
+from random import randint, uniform
 
 
-def show_array():
-    pass
+def show_array(array):
+    print(' '.join([str(i) for i in array]))
 
 
-def fill_array():
-    return None
+def d(x):
+    return x - int(x)
+
+
+def fill_array(n):
+    out = [d(uniform(0, 1))]
+    for i in range(n - 1):
+        out.append(d(out[-1] * 9.5))
+    return out
 
 
 def main():
-    pass
+    arr = fill_array(5)
+    show_array(arr)
 
 
 if __name__ == '__main__':
