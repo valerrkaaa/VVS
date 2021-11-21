235b3a71fffc32d2d6784007509d10abf20e89dd
valerrkaaa <val_g_2001@bk.ru>
2021-11-21 13:26:12 +0500

    реализация проверки метода частот и пар

--- lab1_karimov/lab1_Karimov.py
+++ lab1_karimov/lab1_Karimov.py
@@ -1,3 +1,4 @@
+import math
 from random import randint, uniform
 from pprint import pprint
 
@@ -8,7 +9,7 @@ def array_print(array):
     """
     Вывод в консоль значений одномерного массива
     """
-    print(' '.join([str(i) for i in array]))
+    print(', '.join([str(i) for i in array]))
 
 
 def d(x: float):
@@ -37,6 +38,17 @@ def get_i_from_float(a, i: int):
     return int(a * (10 ** i) % 10)
 
 
+def check_hi_borders(hi: float, hi_lower=3.32, hi_upper=16.9):
+    """
+    Проверка границ доверительного интервала
+    """
+    if hi > hi_lower:
+        if hi < hi_upper:
+            return True, 'Входит в доверительный интревал'
+        return False, 'Больше верхней границы'
+    return False, 'Меньше нижней границы'
+
+
 def test_freq_and_pairs(arr):
     if len(arr) % 2 == 0:
 
@@ -44,28 +56,74 @@ def test_freq_and_pairs(arr):
         # пример: 0.123... -> [1, 2, 3, .., 2 * k]
         e_arr = []
         for a in arr:
-            e_arr.extend([get_i_from_float(a, i) for i in range(1, 2 * k)])
+            e_arr.extend([get_i_from_float(a, i) for i in range(1, 2 * k + 1)])
+        print('\nОдномерный массив:')
+        array_print(e_arr)
 
         # разбиение на пары
         # пример: [1, 2, 3, 4] -> [(1, 2), (3, 4)]
         pairs_arr = [(e_arr[i], e_arr[i + 1]) for i in range(0, len(e_arr) - 1, 2)]
+        print('\nМассив пар:')
+        array_print(pairs_arr)
 
         # Создание матрицы 10 на 10 с подсчётом числа повторений
+        # Пример: [(1, 1), (1, 0), (1, 1)] -> [[0, 0],
+        #                                     [[1, 2]]
         matrix_array = [[0 for _ in range(10)] for _ in range(10)]
         for e in pairs_arr:
             matrix_array[e[0]][e[1]] += 1
+        print('\nСформированная матрица:')
         pprint(matrix_array)
 
+        # Считаем сумму элементов (vi - сумма i-того столбца, vj - сумма j-той строки)
+        # Пример: [[0, 0],
+        #         [[1, 2]] -> [1, 5]
+        vi = []
+        for i in range(len(matrix_array)):
+            vi_sum = 0
+            for j in range(len(matrix_array[i])):
+                vi_sum += matrix_array[j][i] + matrix_array[i][j]
+            vi.append(vi_sum)
+        print('\nvi:')
+        array_print(vi)
+
+        # Считаем величину Хи-квадрат по формуле 1.13
+        p = 0.1
+        pN = p * len(e_arr)
+        sum_i = 0
+        for v in vi:
+            sum_i += (v - pN) ** 2
+        hi = 1 / pN * sum_i
+        print(f'\nХи-квадрат(1.13) = {hi}')
+        _, text = check_hi_borders(hi)
+        print(text)
+
+        # Считаем Хи-квадрат по формуле 1.14
+        p = 0.01
+        pN = p * len(e_arr)
+        sum_i = 0
+        for i in range(10):
+            sum_j = 0
+            for j in range(10):
+                sum_j += (matrix_array[i][j] - pN) ** 2
+            sum_i += sum_j
+        hi = math.sqrt(1 / pN * sum_i)
+        print(f'\nХи-квадрат(1.14) = {hi}')
+        _, text = check_hi_borders(hi)
+        print(text)
     else:
-        return -1
+        print('Число элементов должно быть чётным!')
 
 
 def main():
+    # генерируем массив из 100 чисел:
     arr = fill_array(100)
+    print('\nМассив чисел:')
     array_print(arr)
-    test_freq_and_pairs(arr)
+
+    # тест проверки частот и пар:
+    test_freq_and_pairs(arr=arr)
 
 
 if __name__ == '__main__':
     main()
-    # print(test_freq_and_pairs(0.123456, 6))
